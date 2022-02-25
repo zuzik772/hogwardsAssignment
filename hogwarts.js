f872383c0499808c01a310858b7e214bebe13eaf
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-02-25 15:18:36 +0100

    add search bar

--- hogwarts.js
+++ hogwarts.js
@@ -182,3 +182,26 @@ function sortList(arr, propertyName, direction) {
 
   displayList(list);
 }
+
+// search function
+function searchFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}

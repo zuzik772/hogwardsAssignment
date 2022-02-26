e17c9bdc99dee33e81739cc4db3e6a967c53eeef
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2022-02-26 09:45:13 +0100

    cleanr code but expelled list still not working

--- hogwarts.js
+++ hogwarts.js
@@ -30,17 +30,6 @@ function start() {
     });
   });
 
-  // let listButtons = document.querySelectorAll(".filter");
-  // listButtons.forEach((btn) => {
-  //   btn.addEventListener("click", function () {
-  //     console.log(this.dataset.filter);
-  //     if (this.dataset.filter === "all") {
-  //       displayList(allStudents);
-  //     } else {
-  //       displayList(filteringExpelled(this.dataset.filter));
-  //     }
-  //   });
-  // });
   // sort click
   const sortBtns = document.querySelectorAll("[data-action=sort]");
   sortBtns.forEach((sortBtn) => {
@@ -90,8 +79,7 @@ function prepareObjects(studentObject) {
   student.house = newHouse;
   let studentGender = studentObject.gender;
   student.gender = studentGender;
-  // student.isExpelled = "active";
-  // console.log("student object", student.firstName);
+
   allStudents.push(student);
 
   displayList(allStudents);
@@ -132,11 +120,6 @@ function getImage(fullname) {
   }
 }
 
-// function sameLastName(fullname) {
-//   let lastName = fullname.substring(fullname.lastIndexOf(" ") + 1);
-// //
-// }
-
 function displayList(students) {
   // clear the list
   document.querySelector("#list tbody").innerHTML = "";
@@ -163,26 +146,13 @@ function displayStudent(alumni) {
   } else {
     clone.querySelector("[data-field=status]").textContent = "active";
   }
-  // // remove expelled student
-  // if (isExpelled === true) {
-  //   console.log("student is expelled this time");
-  //   clone.querySelector("[data-field=firstName]").textContent = "";
-  //   clone.querySelector("[data-field=lastName]").textContent = "";
-  //   clone.querySelector("[data-field=middleName]").textContent = "";
-  //   clone.querySelector("[data-field=nickName]").textContent = "";
-  //   clone.querySelector("[data-field=image] img").src = "";
-  //   clone.querySelector("[data-field=gender]").textContent = "";
-  //   clone.querySelector("[data-field=house]").textContent = "";
-  // } else {
-  //   console.log("student is NOT expelled");
-  // }
 
   // student click
   let studentBtn = clone.querySelectorAll("td");
   studentBtn.forEach((btn) => {
     btn.addEventListener("click", function () {
       showPopUp(alumni);
-      // btn.style.backgroundColor = "yellow";
+      btn.style.backgroundColor = "yellow";
     });
   });
 
@@ -191,10 +161,10 @@ function displayStudent(alumni) {
   //   console.table(alumni);
 }
 // filtering house
-function filterList(filter) {
+function filterList(house) {
   let list = allStudents.filter(isStudentHouse);
   function isStudentHouse(student) {
-    if (student.house === filter) {
+    if (student.house === house) {
       return true;
     } else {
       return false;
@@ -207,36 +177,6 @@ function filterList(filter) {
   filteredArray = list;
   // console.table(filteredArray);
   displayList(filteredArray);
-}
-// filter expelled students
-// function filterExpelledList(expelled) {
-//   let list = allStudents.filter(newList);
-//   function isStudentExpelled(student) {
-//     if (student.isExpelled === expelled) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   if (list.length === 0) {
-//     list = allStudents;
-//   }
-//   filteredArray = list;
-//   // console.table(filteredArray);
-//   buildList();
-// }
-
-function filterExpelledList(expelledStatus) {
-  console.log(expelledStatus);
-  allStudents.forEach((student) => console.log(student.isExpelled == expelledStatus));
-  let filteredData = allStudents.filter((student) => String(student.expelled) == expelledStatus);
-
-  const result = words.filter((word) => word.length > 6);
-  // if (!filteredData.length) {
-  //   filteredData = allStudents;
-  // }
-  return filteredData;
 }
 
 // sorting
@@ -282,6 +222,8 @@ function searchFunction() {
 
 function showPopUp(student) {
   document.querySelector("#studentPopUp").classList.remove("hidden");
+  document.querySelector("#studentPopUp [data-action=remove]").disabled = false;
+  document.querySelector("#studentPopUp [data-action=remove]").style.color = "black";
   document.querySelector("#studentPopUp .closebutton").addEventListener("click", function () {
     closePopUp(student);
   });
@@ -299,70 +241,43 @@ function showPopUp(student) {
     expellStudent(student);
   });
 }
-
 function closePopUp(student) {
   document.querySelector("#studentPopUp").classList.add("hidden");
   console.log("student:", student);
   document.querySelector("#studentPopUp [data-action=remove]").textContent = "Expell: " + student.firstName + " " + student.lastName;
   document.querySelector("#studentPopUp [data-action=remove]").style.backgroundColor = "orange";
-
-  return buildList(student);
 }
 
 function expellStudent(expelledStudent) {
+  document.querySelector("#studentPopUp [data-action=remove]").disabled = true;
   document.querySelector("#studentPopUp [data-action=remove]").style.backgroundColor = "red";
+  document.querySelector("#studentPopUp [data-action=remove]").style.color = "white";
   // alert(`${expelledStudent.firstName} has been expelled!`);
   document.querySelector("#studentPopUp [data-action=remove]").innerHTML = "EXPELLED!!!";
-  let expelled = (expelledStudent.isExpelled = true);
+  // let expelled = (expelledStudent.isExpelled = true);
 
   const index = allStudents.indexOf(expelledStudent);
   let expelledStudents = allStudents.splice(index, 1);
   console.log("allstudents length: ", allStudents.length);
   console.log("new expelled Student: ", expelledStudents);
 
+  // doesnt work adds random ammount of people to new list after expelling more than 1 student
   newList.push(expelledStudents);
 
   console.log("what is this: ", newList);
 
-  // buildList(expelledStudents);
-  // buildList(expelledStudents);
-  // expelled =
-  // addStatus(expelled);
-  // document.querySelector("#studentPopUp [data-field=status]").textContent = expelledStudent.isExpelled = true;
-  // if ((expelledStudent.isExpelled = true)) {
-  //   buildList(expelledStudent);
+  // console.log("expelled student is ", expelled);
 
+  // return newList;
+  buildList(expelledStudent);
+  // displayExpelledStudents(newList);
+  // document.querySelector("[data-filter=expelled]").addEventListener("click", displayExpelledStudents);
+  // function displayExpelledStudents() {
+  //   allStudents.filter(buildList(newList));
   // }
-
-  // removeStudentFromList(expelled);
-  console.log("expelled student is ", expelled);
-  // clear the list
-  // document.querySelector("#list tbody").innerHTML = "";
-  return newList;
 }
 
 function buildList() {
   const currentList = allStudents;
   displayList(currentList);
 }
-
-// function buildExpelledList(expelledStudent) {
-//   if (isExpelled === true) {
-//     isExpelled = false;
-//   } else {
-//     isExpelled = true;
-//   }
-// }
-
-// function removeStudentFromList(expelledStudent) {
-//   let removeStudent = expelledStudent.classList.add("hidden");
-//   return removeStudent
-// }
-
-// function addStatus(student) {
-//   if (student.isExpelled === true) {
-//     document.querySelector("#studentPopUp [data-field=status]").textContent = "expelled";
-//   } else {
-//     document.querySelector("#studentPopUp [data-field=status]").textContent = "active";
-//   }
-// }
